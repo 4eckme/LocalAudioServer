751ec834a41c0d02c64c5c4a896acdb2b07827ff
4eckme <60138564+4eckme@users.noreply.github.com>
2026-03-11 15:04:02 +0300

    Cross-platform (Windows+Linux)
    add linux

--- server.py
+++ server.py
@@ -1,11 +1,49 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import socket
-
-from pycaw.pycaw import AudioUtilities
-volume = AudioUtilities.GetSpeakers().EndpointVolume
+import platform
+if platform.system() == 'Windows':
+    from pycaw.pycaw import AudioUtilities
+    volume = AudioUtilities.GetSpeakers().EndpointVolume
+elif platform.system() == 'Linux':
+    import subprocess
 
 def clamp(value, minimum, maximum):
     return min(max(value, minimum), maximum)
+
+class AudioController:
+    
+    def getVolume(self):
+        if platform.system() == 'Windows':
+            return str(round(volume.GetMasterVolumeLevelScalar()*100))
+        elif platform.system() == 'Linux':
+            output = subprocess.run(
+                "amixer get Master | tail -n 1",
+                shell=True,
+                capture_output=True,
+                text=True,
+                check=True
+            ).stdout
+            output = output[:output.index('%')]
+            return str(int(output[output.index('[')+1:]))
+        return None;
+    
+    def setVolume(self, value):
+        value = clamp(int(value), 0, 100)
+        if platform.system() == 'Windows':
+            volume.SetMasterVolumeLevelScalar(value/100, None)
+            return str(round(volume.GetMasterVolumeLevelScalar()*100))
+        elif platform.system() == 'Linux':
+            subprocess.run(
+                f"amixer set Master {value}% | tail -n 1",
+                shell=True,
+                capture_output=True,
+                text=True,
+                check=False
+            )
+            return str(value)
+        return None;
+
+AC = AudioController()
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -19,16 +57,15 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(str(round(volume.GetMasterVolumeLevelScalar()*100)).encode('utf-8'))
+            self.wfile.write(AC.getVolume().encode('utf-8'))
         elif self.path.startswith('/?set='):
             split = self.path.split("=")
             if len(split) == 2 and all('0' <= char <= '9' for char in split[1]):
-                value = clamp(int(split[1])/100,0,1)
-                volume.SetMasterVolumeLevelScalar(value, None)
+                value = AC.setVolume(split[1])
                 self.send_response(200)
                 self.send_header('Content-type', 'text/plain')
                 self.end_headers()
-                self.wfile.write(str(round(volume.GetMasterVolumeLevelScalar()*100)).encode('utf-8'))
+                self.wfile.write(value.encode('utf-8'))
         elif self.path == '/icon.png':
             self.send_response(200)
             self.send_header('Content-type', 'image/png')
@@ -37,9 +74,17 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                 self.wfile.write(file.read())
 
 def run_server(port=8088):
+    local_ip = '127.0.0.1'
     hostname = socket.gethostname()
     ips = socket.gethostbyname_ex(hostname)[2]
-    local_ip = '127.0.0.1'
+    if 'Linux' == platform.system():
+        ips = list(set(ips + subprocess.run(
+            "hostname -I",
+            shell=True,
+            capture_output=True,
+            text=True,
+            check=True
+        ).stdout.split()))
     for ip in ips:
         if ip.startswith('192.168.'):
             local_ip = ip
@@ -56,4 +101,3 @@ def run_server(port=8088):
 
 if __name__ == '__main__':
     run_server()
-
